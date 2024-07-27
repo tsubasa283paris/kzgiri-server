22b878340ececb3015ced4c59ef24b2b74ff6503
tsubasa283paris <tsubasa283paris@gmail.com>
2024-07-28 00:28:04 +0900

    Apply prettier to .rb files and Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_07_22_141435) do
-  create_table "topics", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "topics",
+               charset: "utf8mb4",
+               collation: "utf8mb4_0900_ai_ci",
+               force: :cascade do |t|
     t.string "text", default: "", null: false
     t.datetime "published_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
